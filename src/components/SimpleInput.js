e78cc33c90380379b2e8baa35805bb71e292fe31
sistiri <sistiri@yahoo.com>
2022-02-11 10:39:38 +0100

    Managing Overall Form Validity with useEffect() Hook

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false)
+
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  useEffect(() => {
+    if(enteredNameIsValid) {
+      setFormIsValid(true)
+    } else {
+      setFormIsValid(false)
+    }
+  }, [enteredNameIsValid])
 
   const namInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -47,7 +57,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
